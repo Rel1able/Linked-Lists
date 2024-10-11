56a0cb99bdda9c5b771d6667154478ad9991dd3f
Yaroslav <yaroslav.shebovovkk@gmail.com>
2024-10-11 19:47:21 +0200

    added prepend, size, handNode and tail methods

--- index.js
+++ index.js
@@ -23,6 +23,56 @@ export class LinkedList{
         }
 
     }
+    prepend(value) {
+        let newNode = new Node(value);
+        newNode.nextNode = this.head;
+        this.head = newNode;
+    }
+    size() {
+        let count = 0;
+        let node = this.head;
+        while (node) {
+            count ++;
+            node = node.nextNode;
+        }
+        return count;
+    }
+    headNode() {
+        return this.head;
+    }
+    tail() {
+        let lastNode = this.head;
+        if (lastNode) {
+            while (lastNode.nextNode) {
+                lastNode = lastNode.nextNode;
+            }
+        }
+        return lastNode;
+    }
+    at(index) {
+        
+    }
+    pop() {
+        
+    }
+    contains(value) {
+        
+    }
+    find(value) {
+        
+    }
+    toString() {
+        let current = this.head;
+        let finalString = "";
+
+        while (current) {
+            
+            finalString += `( ${current.value} )-> `;
+            current = current.nextNode;
+        }
+        finalString += null;
+        return finalString;
+    }
 }
 
 
